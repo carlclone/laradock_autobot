d741e5f6cbbab8536719aea9800f6e9eade6d2b7
677 <906561974@qq.com>
2017-08-28 13:08:21 +0800

    add database script

--- add_database.php
+++ add_database.php
@@ -5,3 +5,11 @@
  * Date: 28/08/2017
  * Time: 10:30
  */
+
+/*exec('cd '.$laradockFolder.' && docker-compose exec mysql bash');
+exec('mysql -uroot -proot');
+exec('create database '.$domain.';');*/
+
+$pdo = new PDO("mysql:host=127.0.0.1;","root","root");
+$pdo->exec('create database '.$domain);
+var_dump('数据库创建完成');
